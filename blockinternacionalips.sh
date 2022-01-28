daab7adfc2235c850b8cf7dc919b6977947183dd
luiznascimento <t0p@me.com>
2022-01-28 11:03:24 -0300

    Update blockinternacionalips.sh

--- blockinternacionalips.sh
+++ blockinternacionalips.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-#
-#  @author: luiz nascimento
-#  @name: blockinternacionalips.sh
-#
-
 
 ATKFILE='/tmp/ip-attackers.txt'
-iptables -L -n  | egrep DROP | awk '{ print $4 }' > ${ATKFILE}
+/usr/sbin/iptables -L -n  | egrep DROP | awk '{ print $4 }' > ${ATKFILE}
 
 for ATTACKER in $(for x in $(egrep "SASL LOGIN" /var/log/zimbra.log | cut -d\[ -f3 | cut -d\] -f1 | sort | uniq | awk '{ print $1 }' );
     do 
-        echo $x - $(geoiplookup $x)
+        echo $x - $(/usr/bin/geoiplookup $x)
 done | egrep -v BR | awk '{ print $1 }' ) 
         do 
 			if [[ $(egrep -q "^${ATTACKER}$" ${ATKFILE} ; echo $?) -eq '0' ]];
@@ -18,6 +13,6 @@ done | egrep -v BR | awk '{ print $1 }' )
 				echo "${ATTACKER} - IP found"
 			else
 				echo "${ATTACKER} - IP not found"
-				iptables -A INPUT -p tcp -s ${ATTACKER} -j DROP
+				/usr/sbin/iptables -A INPUT -p tcp -s ${ATTACKER} -j DROP
 			fi
 done
