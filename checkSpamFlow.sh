198921637397c95938bde2fbc17fa0cc7f514522
luiznascimento <t0p@me.com>
2022-02-09 14:08:30 -0300

    Update checkSpamFlow.sh

--- checkSpamFlow.sh
+++ checkSpamFlow.sh
@@ -1,23 +1,88 @@
 #!/bin/bash
 #
-#  @nome: checkSpamFlow.sh
+#  @nome: checkSpam.sh
 #  @data: 21/10/2021
 #  @autor: Luiz Nascimento
-#  @objectiv: gerar um contador de recipientes separados por from por minuto.
+#  @objetivo: gerar um contador de recipientes separados por from por minuto.
 #
-
 #variaveis
 
 dir="/tmp/countrecipients/"
-quantidadedelog="5"
+counterlog=${dir}onelog
+totalcount=${dir}totalcount
+#domspipefile=${dir}domspipefile
 
-#limpa antigos
+mkdir -p ${dir} 2> /dev/null
 
-ls -tl | egrep -v total | sed '1,${quantidadedelog}d' | awk '{ print $9 }'  | xargs rm -f 2> /dev/null
+oneago=$(date "+%R" -d "1 min ago")
+twoago=$(date "+%R" -d "2 min ago")
+threeago=$(date "+%R" -d "3 min ago")
+fourago=$(date "+%R" -d "4 min ago")
+fiveago=$(date "+%R" -d "5 min ago")
 
-data_search=$(echo $(date | awk '{ print $2" "$3" "$4 }') | awk -F: '{ print $1":"$2}')
-minute_search=$(echo ${data_search} | awk -F: '{ print $2 }')
+oneminute=$(echo ${oneago} | cut -d\: -f2)
+twominute=$(echo ${twoago} | cut -d\: -f2)
+threeminute=$(echo ${threeago} | cut -d\: -f2)
+fourminute=$(echo ${fourago} | cut -d\: -f2)
+fiveminute=$(echo ${fiveago} | cut -d\: -f2)
 
-egrep "${data_search}" /var/log/zimbra.log | egrep "from=<" | egrep "@specific-domain.com.br" | awk -F'from=<' '{ print $2 }'  | awk '{ print $1" "$3}' | egrep ESMTP -v | sed 's/>, nrcpt=/ /' > ${dir}/${minute_search}
+onehour=$(echo ${oneago} | cut -d\: -f1)
+twohour=$(echo ${twoago} | cut -d\: -f1)
+threehour=$(echo ${threeago} | cut -d\: -f1)
+fourhour=$(echo ${fourago} | cut -d\: -f1)
+fivehour=$(echo ${fiveago} | cut -d\: -f1)
 
-for x in $( awk '{ print $1 }' /tmp/out | sort | uniq ); do echo ${x} $(echo $(egrep $x /tmp/out | awk '{ print $2 }' | paste -sd+ | bc -l )); done
+echo -n > ${counterlog} > ${totalcount}
+
+#doms=(for dom in $(su -l zimbra -c "zmprov -l gad" |egrep -v "qualmbox01.a.inova.com.br|qualmbox01.aws.inova.com.br") ; do echo -n "${dom}"; done )
+
+doms=$( su -l zimbra -c "zmprov -l gad" | egrep -v "qualmbox01.a.inova.com.br|qualmbox01.aws.inova.com.br" )
+domspipe=$(for dom in ${doms}
+do
+    let i=$i+1 ;
+    count_doms=$(echo ${doms} | wc -w)
+    if [[ ${i} -eq ${count_doms} ]]
+    then 
+        echo -n "$dom"
+    else 
+        echo -n "$dom|"
+    fi
+done)
+#
+#for domain in ${doms}
+#do
+#    let i+i=1
+#    if [[ $i -eq $(echo ${doms} | wc -w) ]]
+#    then 
+#        echo -n "$domain"
+#    else 
+#        echo "$domain|"
+#    fi
+#done > 
+#
+#echo Doms: ${doms}
+#echo DomsPipe: ${domspipe}
+
+#exit 0
+
+for dom in ${doms}
+do
+
+for d in one two three four five
+do
+	eval egrep " \${${d}hour}:\${${d}minute}" /var/log/zimbra.log | egrep "from=<" | egrep "${domspipe}" | awk -F'from=<' '{ print $2 }'  | awk '{ print $1" "$3}' | egrep ESMTP -v | egrep "nrcpt=" | sed 's/>, nrcpt=/ /' 
+done
+done >> ${counterlog}
+
+
+for count in $(awk '{ print $1 }' ${counterlog} | sort | uniq ); do echo $( egrep "^${count}" ${counterlog} | awk '{ print $2 }' | paste -sd+ | bc -l) - $count ; done | sort -n > ${totalcount} 
+
+for mail in $(awk '{ print $3}' ${totalcount} )
+       do 
+          if [[ "$(egrep "${mail}$" ${totalcount} | awk '{ print $1 }')" -gt "100" ]]
+          then
+		echo Bloqueia "${mail}" $(egrep "${mail}$" ${totalcount}| awk '{ print $1 }')
+	  else
+		echo menor: "${mail}" $(egrep "${mail}$" ${totalcount} | awk '{ print $1 }')
+	  fi
+done
