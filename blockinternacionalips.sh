b63818e8e31bc5bd5fcb8b066e3235dabcd48e0f
luiznascimento <t0p@me.com>
2022-03-17 15:48:26 -0300

    Update blockinternacionalips.sh

--- blockinternacionalips.sh
+++ blockinternacionalips.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 #
+#  @author: luiz nascimento
 #  @name: blockinternacionalips.sh
-#  @author: luiz.nascimento
 #
 
-#tmpfile
+GEOI=/usr/bin/geoiplookup
+IPT=/usr/sbin/iptables
 ATKFILE='/tmp/ip-attackers.txt'
 
-#blockedips
-/usr/sbin/iptables -L -n  | egrep DROP | awk '{ print $4 }' > ${ATKFILE}
+$IPT -L -n  | egrep DROP | awk '{ print $4 }' > ${ATKFILE}
 
-for ATTACKER in $(for x in $(egrep "SASL LOGIN" /var/log/zimbra.log | cut -d\[ -f3 | cut -d\] -f1 | sort | uniq | awk '{ print $1 }' );
+
+for ATTACKER in $(for x in $(egrep "SASL LOGIN" /var/log/zimbra.log | cut -d\[ -f3 | cut -d\] -f1 | sort | uniq | awk '{ print $1 }'| grep -v '64.251.25.26' );
     do 
-        echo $x - $(/usr/bin/geoiplookup $x)
+        echo $x - $($GEOI $x)
 done | egrep -v BR | awk '{ print $1 }' ) 
         do 
-			if [[ $(egrep -q "^${ATTACKER}$" ${ATKFILE} ; echo $?) -eq '0' ]];
-			then
-				echo "${ATTACKER} - IP found"
-			else
-				echo "${ATTACKER} - IP not found"
-				/usr/sbin/iptables -A INPUT -p tcp -s ${ATTACKER} -j DROP
-			fi
+                        if [[ $(egrep -q "^${ATTACKER}$" ${ATKFILE} ; echo $?) -eq '0' ]];
+                        then
+                                echo "${ATTACKER} - IP found"
+                        else
+                                echo "${ATTACKER} - IP not found"
+                                $IPT -A INPUT -p tcp -s ${ATTACKER} -j DROP
+                        fi
 done
