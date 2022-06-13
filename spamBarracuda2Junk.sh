9e1ed2d2afafc62777b3699bb28c78db1e1c43bd
luiznascimento <t0p@me.com>
2022-06-13 18:32:45 -0300

    Update spamBarracuda2Junk.sh

--- spamBarracuda2Junk.sh
+++ spamBarracuda2Junk.sh
@@ -4,14 +4,14 @@
 # @author: Vitor Bento
 #
 
-allAccounts=$(ldapsearch -x -H ldap://sepacmbox01.aws.inova.com.br:389 -D uid=zimbra,cn=admins,cn=zimbra -w LK3W3UPw -LLL \
+allAccounts=$(ldapsearch -x -H ldap://xxxxxxxxxxxxxxxxxxxxxxxx:389 -D uid=zimbra,cn=admins,cn=zimbra -w xxxxxxxxxxx -LLL \
 '(&(ObjectClass=zimbraAccount)(zimbraAccountStatus=active))' | grep -i zimbraMailDeliveryAddress | awk '{print$2}');
 
 varNum=0
 
 for conta in $allAccounts
 do
-   filtros=$(ldapsearch -o ldif-wrap=no -x -H ldap://sepacmbox01.aws.inova.com.br:389 -D uid=zimbra,cn=admins,cn=zimbra -w LK3W3UPw -LLL '(&(ObjectClass=zimbraAccount)(zimbraMailDeliveryAddress='$conta'))' | grep -i zimbraMailSieveScript | awk '{print$2}' | base64 -d | grep '#' | awk '{print$2}')
+   filtros=$(ldapsearch -o ldif-wrap=no -x -H ldap://xxxxxxxxxxxxxxxxxxx:389 -D uid=zimbra,cn=admins,cn=zimbra -w xxxxxxxxxxx -LLL '(&(ObjectClass=zimbraAccount)(zimbraMailDeliveryAddress='$conta'))' | grep -i zimbraMailSieveScript | awk '{print$2}' | base64 -d | grep '#' | awk '{print$2}')
    for fNomes in $filtros;do
          if [[ $fNomes = "spamTagBarracuda" ]]
             then
