87844635fd557c2e4218a32326164915793c3df4
luiznascimento <t0p@me.com>
2022-08-01 14:58:51 -0300

    Update blockinternacionalips.sh

--- blockinternacionalips.sh
+++ blockinternacionalips.sh
@@ -8,11 +8,11 @@ GEOI=/usr/bin/geoiplookup
 IPT=/usr/sbin/iptables
 ATKFILE='/tmp/ip-attackers.txt'
 
-if [[ -e !${GEOI} ]]
+if [[ -e ${GEOI} ]]
 then
-	exist=1
+        exist=1
 else
-	echo Por favor instale o pacote do Geo IP
+    echo Por favor instale o pacote do Geo IP
     exit 0
 fi
 
@@ -20,16 +20,17 @@ fi
 $IPT -L -n  | egrep DROP | awk '{ print $4 }' > ${ATKFILE}
 
 
-for ATTACKER in $(for x in $(egrep "SASL LOGIN" /var/log/zimbra.log | cut -d\[ -f3 | cut -d\] -f1 | sort | uniq | awk '{ print $1 }'| grep -v '64.251.25.26' );
-    do 
+for ATTACKER in $(for x in $(egrep "SASL LOGIN" /var/log/zimbra.log | cut -d\[ -f3 | cut -d\] -f1 | sort | uniq | awk '{ print $1 }'| egrep -v 'IP-A-SER-IGNORADO01|IP-A-SER-IGNORADO02' );
+    do
         echo $x - $($GEOI $x)
-done | egrep -v BR | awk '{ print $1 }' ) 
-        do 
+done | egrep -v "BR|IP Address not found" | awk '{ print $1 }' )
+        do
                         if [[ $(egrep -q "^${ATTACKER}$" ${ATKFILE} ; echo $?) -eq '0' ]];
                         then
                                 echo "${ATTACKER} - IP found"
                         else
                                 echo "${ATTACKER} - IP not found"
                                 $IPT -A INPUT -p tcp -s ${ATTACKER} -j DROP
+                                echo "$(date) - $(hostname) - Bloqueado: ${ATTACKER} - Origem: $(geoiplookup ${ATTACKER} | awk -F'GeoIP Country Edition: ' '{ print $2 }')" >> /var/log/ips-blocked.log
                         fi
 done
