19e48c193e1c5b9a99e5388be2e347c8ec7832c1
luiznascimento <t0p@me.com>
2023-01-31 16:18:01 -0300

    Update last7days.sh

--- last7days.sh
+++ last7days.sh
@@ -22,7 +22,7 @@ for D in $(seq 23 30);
     for H in $(seq - 00 23);
       do
       	#Lista de froms por dia
-        egrep "May ${d}" /tmp/nrpc-all.txt| egrep "@saocristovao.com.br>" | awk '{ print $7 }' | sort | uniq ) > ${FROMLISTBYDAY}-${D}
+        egrep "May ${d}" /tmp/nrpc-all.txt| egrep "@dominio-cliente>" | awk '{ print $7 }' | sort | uniq ) > ${FROMLISTBYDAY}-${D}
 		for FROM in $(cat NRPCFROMPERDAY)
         echo $(egrep "May ${d} ${h}" /tmp/nrpc-all.txt| egrep "${FROM}" | awk '{ print $9 }' | awk -F\= '{ print $2  }' | paste -sd+ | bc -l)
         #  > ${NRPCFROMPERDAY}-${D}-${H}
