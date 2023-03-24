2b607e04047db239bf70786191dac311921f21c2
luiznascimento <007.top.secret@gmail.com>
2023-03-24 08:43:15 -0300

    Adjust timeout to stabilish connection

--- ssh-tunnel/sshtunnel-include.sh
+++ ssh-tunnel/sshtunnel-include.sh
@@ -6,5 +6,6 @@
 #  @email: luiz.nascimento at penso.com.br
 #
 #
-
-ssh 10.30.0.243 -L 2222:10.30.8.21:22
+ 
+ssh -tt 10.30.0.243 -L 2222:10.30.8.21:22
+sleep 2678400
--- ssh-tunnel/sshtunnel.sh
+++ ssh-tunnel/sshtunnel.sh
@@ -7,15 +7,14 @@
 #
 #
 
-PATH=${pwd}
-
-echo ${PATH}
+DIR=$(dirname $0)
 
 # Define a expressão regular para o formato de endereço IP
 IPREGEX='^([0-9]{1,3}\.){3}[0-9]{1,3}$'
 
   # Verifica se o argumento corresponde ao formato do IP
-  if [[[ $HOSTA =~ $IPREGEX ]] AND [[ $HOSTB =~ $IPREGEX ]] AND [[ $HOSTC =~ $IPREGEX ]]]
-  then
-     ssh fw -L 2222:0.0.0.0:2222 'bash -s' < sshtunnel-include.sh
-  fi
+     ps aux | egrep "L 2222" | awk '{ print $2 }' | xargs kill > /dev/null
+     sleep 10;
+     ssh fw -tt -L 2222:0.0.0.0:2222 'bash -s' <(${DIR}/sshtunnel-include.sh; sleep 3600000)
+     #sleep 10
+     
